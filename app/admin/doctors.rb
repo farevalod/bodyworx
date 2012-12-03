b472ed5ec7f7447e030b18565e3149f0089f1c68
Francisco Arevalo <farevalod@gmail.com>
2012-12-03 15:01:56 -0300

    Added doctors form

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Doctor do
-  
+	form do |f|
+	      f.inputs "Details" do
+			f.input :clinic
+			f.input :first, :label => "Nombre"
+			f.input :last, :label => "Apellido"
+			f.input :dob, :label => "Fecha de nacimiento", :start_year => 1940, :end_year => 1990
+		  end
+		f.buttons
+	end
 end

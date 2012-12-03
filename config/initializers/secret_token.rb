592a347170d97d4eb1e169c05c163227f9c0b887
Francisco Arevalo <farevalod@gmail.com>
2012-12-03 11:47:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodyworx::Application.config.secret_token = '79f8c8cdd96170a7410a33bc5e7e3008371584def946bb85fb5478bd66bdb30ce3a7f1a91036279feab23f40108373f39c2f2e64ad77ef91bda1568b20481ef7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bodyworx::Application.config.session_store :cookie_store, key: '_bodyworx_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bodyworx::Application.config.session_store :active_record_store

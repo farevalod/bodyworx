1516f8a831d02b081b5f93d0e3f9e53119bd0219
4ws <admin@4ws.cl>
2013-07-23 21:06:50 -0400

    index patients and form

--- app/admin/patients.rb
+++ app/admin/patients.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Patient do
-  
-end
+ index do
+      column :first, :label => "Nombre"
+      column :last, :label => "Apellido"
+      default_actions
+    end
+	form do |f|
+	      f.inputs "Details" do
+			f.input :first, :label => "Nombre"
+			f.input :last, :label => "Apellido"
+		  end
+		f.buttons
+	end
+end 

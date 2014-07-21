892f27344dd6b7c2ee49c4b09c2d7679931ae68b
Francisco Arevalo <farevalod@gmail.com>
2014-07-21 18:52:37 -0400

    Factura # name

--- app/models/factura_venta.rb
+++ app/models/factura_venta.rb
@@ -3,4 +3,8 @@ class FacturaVenta < ActiveRecord::Base
   has_many :transacts
   attr_accessible :factura_id, :monto, :num
   accepts_nested_attributes_for :transacts
+
+  def name
+	  return "Factura #"+num.to_s
+  end
 end
